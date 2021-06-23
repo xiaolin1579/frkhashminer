05b8b1c626cedfa4b3372e995651a0344d4976dc
chicken <sithslow@gmail.com>
2021-06-22 21:32:42 -0400

    fix redeclaration

--- frkhash/lib/frkhash/frkhash.cpp
+++ frkhash/lib/frkhash/frkhash.cpp
@@ -1,11 +1,6 @@
 // frkhash: C/C++ implementation of Frankohash, the Expanse Proof of Work algorithm.
 // Copyright 2021-Present Christopher Franko.
 // Licensed under the Apache License, Version 2.0.
-/*
-MixHash == keccak512(header_hash, nonce)
-FinalHash == keccak256(MixHash)
-*/
-#include "frkhash-internal.hpp"
 
 #include "../support/attributes.h"
 #include "bit_manipulation.h"
@@ -24,8 +19,7 @@ namespace frkhash
 namespace
 {
 
-inline hash512 hash_seed(const hash256& header_hash, uint64_t nonce) noexcept
-{
+inline hash512 hash_seed(const hash256& header_hash, uint64_t nonce) noexcept{
     nonce = le::uint64(nonce);
     uint8_t init_data[sizeof(header_hash) + sizeof(nonce)];
     std::memcpy(&init_data[0], &header_hash, sizeof(header_hash));
@@ -34,25 +28,20 @@ inline hash512 hash_seed(const hash256& header_hash, uint64_t nonce) noexcept
     return keccak512(init_data, sizeof(init_data));
 }
 
-inline hash256 hash_final(const hash512& seed)
-{
+inline hash256 hash_final(const hash512& seed){
     uint8_t final_data[sizeof(seed)];
     std::memcpy(&final_data[0], seed.bytes, sizeof(seed));
     return keccak256(final_data, sizeof(final_data));
 }
-
 }  // namespace
 
-result hash(const hash256& header_hash, uint64_t nonce) noexcept
-{
+result hash(const hash256& header_hash, uint64_t nonce) noexcept{
     const hash512 seed = hash_seed(header_hash, nonce);
     const hash256 mix_hash = hash256_from_bytes64(seed.bytes);
     return {hash_final(seed), mix_hash};
 }
 
-search_result search(const hash256& header_hash,
-    const hash256& boundary, uint64_t start_nonce, size_t iterations) noexcept
-{
+search_result search(const hash256& header_hash, const hash256& boundary, uint64_t start_nonce, size_t iterations) noexcept{
     const uint64_t end_nonce = start_nonce + iterations;
     for (uint64_t nonce = start_nonce; nonce < end_nonce; ++nonce)
     {
